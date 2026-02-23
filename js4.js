2770aa97ef39598a9d50d5885fed6b49c1b021ae
Gayane <111857936+PoghosyanGayane@users.noreply.github.com>
2026-02-23 16:46:48 +0400

    js4.js

--- js4.js
+++ js4.js
@@ -1,7 +1,12 @@
+"use strict";
+
 // TASK 6
 // Fetch data from a public API and render the result on the page
 
-"use strict";
+const main = document.querySelector("main")
+const section = document.createElement("section");
+main.appendChild(section);
+
 const url = "https://api.open-meteo.com/v1/forecast?latitude=40.18&longitude=44.51&hourly=temperature_2m";
 
 fetch(url)
@@ -9,11 +14,14 @@ fetch(url)
   .then(data => {
     const hourlyData = data.hourly;
     const scale = data.hourly_units.temperature_2m;
-    let i;
-    for (i = 0; i < hourlyData.time.length & i <hourlyData.temperature_2m.length; i++) {
+
+    for (let i = 0; i < hourlyData.time.length & i <hourlyData.temperature_2m.length; i++) {
         const time = hourlyData.time[i];
         const temperature = hourlyData.temperature_2m[i];
-        console.log(`weather at ${time} is ${temperature}${scale}`);
+        
+        const p = document.createElement("p");
+        p.innerHTML = `weather at ${time} is ${temperature}${scale}`;
+        section.appendChild(p);
     }
   })
-  .catch(error => console.error("Error:", error));
+  .catch(error => console.error("error:", error));
